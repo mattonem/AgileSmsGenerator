549cd8e559b7464c801f62be35893f8fb3b1f84b
bertrandnau <bertrand.nau@minesdedouai.fr>
2013-12-11 13:51:36 +0100

    3 tests fonctionnels pour la classe SMSDefault

---
+++ src/core/StyleSMS.java
@@ -0,0 +1,10 @@
+package core;
+
+public interface StyleSMS {
+
+	String generateMessage();
+	
+	void changeParameter(String nomDestinataire);
+	// void rajouterParametre(String nom,, Boolean estAmiProche);
+	
+}
---
+++ src/core/StyleSmsNewYear.java
@@ -0,0 +1,9 @@
+package core;
+
+public class StyleSmsNewYear extends StyleSMSDefault {
+	@Override
+	public String generateMessage() {
+		// TODO Auto-generated method stub
+		return "Bonne annee"+nameOfDestinataire;
+	}
+}
